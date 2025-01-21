39b7c677c691ead520cb2e8b7ac666f8ebe11149
liuchuan <liuchuan.huang@flashwire.com>
2025-01-21 20:08:24 +0800

    feat: add vitest

---
+++ test/utils.test.ts
@@ -0,0 +1,10 @@
+import { it, expect, describe } from 'vitest';
+
+describe('utils test', () => {
+    it('100% test', () => {
+        expect(1).toBe(1);
+    });
+    it('add', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
